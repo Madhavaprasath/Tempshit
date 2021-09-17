b2a7893e23ab429ed28a15d61fb1a6193008183e
Madhava <Madhavprasath088@gmail.com>
2021-09-17 23:10:35 +0530

    Added a new file
    The name is aravind.py which is used in kawrgs

---
+++ Project/Aravind.py
@@ -0,0 +1,23 @@
+def add_numbers(*num):
+ sum=0
+ total_numbers=0
+ for i in sum:
+     sum+=i
+     total_numbers+=1
+ return [sum,total_numbers]
+
+def main():
+  numbers=[]
+  total=int(input("total_numbers"))
+  for i in range(0,total):
+      num=int(input("enter the num"))
+      numbers.append(num)
+  print(numbers)
+  aver=add_numbers(numbers)
+  final=aver[0]/aver[1]
+  print("The average of numbers are ",final)
+         
+
+main()
+
+
